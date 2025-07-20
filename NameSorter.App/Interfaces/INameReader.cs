134bdcc39a8bf993da216111916195b46256c015
Cloud Nexi <nexicloud@gmail.com>
2025-07-20 17:21:06 +1000

    [NS-2] Create contract and services with test case for NameReader and PersonName.

---
+++ NameSorter.App/Interfaces/INameReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NameSorter.App.Models;
+
+namespace NameSorter.App.Interfaces
+{
+    /// <summary>
+    /// Interface contract to be consumed mocking and feature extention
+    /// </summary>
+    public interface INameReader
+    {
+        List<PersonName> ReadNames(string filePath);
+    }
+}
