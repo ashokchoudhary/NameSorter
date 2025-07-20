9c17d90a199386f8b7fd01df1bf17cf60ce02c3b
Cloud Nexi <nexicloud@gmail.com>
2025-07-20 16:20:14 +1000

    [NS-2] Model creation and readme update

---
+++ NameSorter.App/Models/PersonalName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter.App.Models
+{
+    /// <summary>
+    /// Personal name models. 
+    /// Assumption that name with be splited by ' ' (space)
+    /// </summary>
+    public class PersonName
+    {
+        public List<string> GivenNames { get; }
+        public string LastName { get; }
+
+        public PersonName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                throw new ArgumentException("Name cannot be null or empty.");
+
+            var parts = fullName.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            // Check name has minimum 2 parts to be considered as valid name.
+            if (parts.Length < 2)
+                throw new ArgumentException("Must have at least one given and last name.");
+
+            LastName = parts.LastOrDefault();
+            GivenNames = parts.Take(parts.Length - 1).ToList();
+        }
+
+        /// <summary>
+        /// Methods to concat the names again using override ToString()
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Join(" ", GivenNames.Append(LastName));
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NameSorter
+	
+	The Goal: Name Sorter The Goal: Name Sorter Build a name sorter. Given a set of names, order that set first by last name, then by any given names the person may have. A name must have at least 1 given name and may have up to 3 given names. 
+	
+	
+# Project Structure
+	/NameSorter
+		/NameSorter.App              // Main console app
+		/NameSorter.Tests            // Unit tests
+		sorted-names-list.txt        // Generate output file
+		unsorted-names-list.txt      // Input file
+		appveyor.yml                 // AppVeyor pipeline yml config
